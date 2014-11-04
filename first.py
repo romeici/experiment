7b326e5c03148cc35238bcfad5f763e45cd8b6bc
xums <romeici@163.com>
2014-11-04 22:38:39 +0800

    new contents

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 print 'I\'m ok.'
 
 def my_abs(x):
@@ -85,3 +85,98 @@ def fact(n):
 
 print fact(10)
 print fact(5)
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print b
+        a, b = b, a + b
+        n = n + 1
+fib(10)
+
+g = (x * x for x in range(10))
+print g.next()
+print g.next()
+for n in g:
+	print n
+	
+l = [x*x for x in range(10)]
+for n in l:
+	print n
+	
+def fib_1(max,beg1 = 0,beg2 = 1):
+	n, a, b = 0,beg1,beg2
+	while n < max:
+		print b
+		a, b = b, a + b
+		n = n + 1
+		
+fib_1(5)		
+fib_1(5, 0, 1)
+fib_1(5, 0, 2)
+# map(f,[])  高阶函数
+map(fib_1, [1,2,3,4,5])
+
+L = ['Hello', 'World', 'IBM', 'Apple']
+for n in L:
+	print [n.lower()]
+
+print [s.lower() for s in L]
+
+print sorted([36, 5, 12, 9, 21])
+
+def reversed_cmp(x, y):
+    if x > y:
+        return -1
+    if x < y:
+        return 1
+    return 0
+	
+print sorted( [6,3,8,5,4,7,1], reversed_cmp)
+
+def cmp_ignore_case(s1, s2):
+    u1 = s1.upper()
+    u2 = s2.upper()
+    if u1 < u2:
+        return -1
+    if u1 > u2:
+        return 1
+    return 0
+	
+print sorted(['about', 'bob', 'Zoo', 'Credit'], cmp_ignore_case)	
+
+def now():
+	print '2013-12-25'
+	
+f = now
+f()
+print f.__name__
+print now.__name__
+
+print int('12345', 10)
+print int('12345', 16)
+#偏函数
+def int2(x, base=2):
+    return int(x, base)
+	
+import urllib2
+#encoding = utf-8
+class Crawler:
+    def main(self):
+        #req = urllib2.Request('http://www.baidu.com/')
+        #req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0)')
+        #urllib2.socket.setdefaulttimeout(10) # 超时10秒
+        #page = urllib2.urlopen(req)
+        page = urllib2.urlopen('http://www.baidu.com', timeout=10)
+        data = page.read()
+        print data
+        print len(data)  #计算字节长度
+	
+if __name__ == '__main__':
+    me=Crawler()
+    me.main()
+
+
+import os, sys
+os.system('curl ' + url)
+os.system('curl -D baidu ' + url)	
