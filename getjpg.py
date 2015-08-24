0f1efb99dd65397b8a5d23075776e762d244eecb
xums <xums@ubuntu.(none)>
2015-08-24 20:47:56 +0800

    get jpg from internet

---
+++ getjpg.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+
+from urllib.request import urlopen 
+from urllib.request import urlretrieve
+
+import re
+
+def getHtml(url):
+    page = urlopen(url)
+    html = page.read()
+    return html
+
+def getImg(html):
+    html = html.decode('utf-8')
+    reg = r'src="(.+?\.jpg)" pic_ext'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre,html)
+    x = 0
+    for imgurl in imglist:
+        urlretrieve(imgurl,'%s.jpg' % x)
+        x+=1
+
+html = getHtml("http://tieba.baidu.com/p/2460150866")
+
+print(getImg(html))
